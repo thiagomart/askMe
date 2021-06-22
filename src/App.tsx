f5ab383c02768aed4f3344c9d1de79c6552ba200
thiagomart <thiagomsl@gmail.com>
2021-06-22 20:40:47 -0300

    style(project): update style project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 
-import { Home } from "./pages/Home";
+// import { Home } from "./pages/Home";
+import { NewRoow } from "./pages/NewRoom";
+
 function App() {
   return (
-    <Home />
+    <NewRoow />
     
   );
 }
