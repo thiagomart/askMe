a12d982416a7a8a8813634a3ff158ca05e3115fe
thiagomart <thiagomsl@gmail.com>
2021-06-22 23:58:04 -0300

    chore(project): auth functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 
-// import { Home } from "./pages/Home";
-import { NewRoow } from "./pages/NewRoom";
+import { BrowserRouter, Route } from 'react-router-dom' 
+import { Home, NewRoom } from './pages';
+import { AuthContextProvider } from './contexts/AuthContext';
+
 
 function App() {
-  return (
-    <NewRoow />
-    
+ return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
